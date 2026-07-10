4a5f81be1df321cac84b2ffc7462403221a45d12
Peter <konnectwithpeter@gmail.com>
2026-07-10 11:38:56 +0300

    Still achanges for the ssl

--- sc-server/config/settings.py
+++ sc-server/config/settings.py
@@ -154,3 +154,7 @@ EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True").lower() == "true"
+
+# Reverse Proxy / SSL configuration
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
